20c0f46f5ccd2b6e4ade339c88be9d0787fe0740
beyazkayaselena-png <beyazkayaselena@gmail.com>
2026-04-15 22:27:22 +0900

    Baekjoon2581.py
    Implement prime number finding between two integers.

---
+++ Baekjoon2581.py
@@ -0,0 +1,25 @@
+import sys
+
+m = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
+
+primes = []
+
+for i in range(m, n + 1):
+    if i < 2:
+        continue
+    
+    is_prime = True
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        primes.append(i)
+
+if not primes:
+    print(-1)
+else:
+    print(sum(primes))
+    print(min(primes))
